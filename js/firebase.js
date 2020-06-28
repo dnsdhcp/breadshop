a3031c320c97e7630f246d063c8f19d104e1f091
qseft <t107590039@ntut.org.tw>
2020-06-29 02:42:18 +0800

    基礎firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,41 @@
-firebase.initializeApp({     projectId: "fuckyou-8dbb6"   });    var db = firebase.firestore(); var ref = db.collection('fruit').doc('apple');  function storedata(){     ref.set({     total: 500,     good: 480,     sale: 330     }).then(() => {     console.log('set data successful');     }); }
+firebase.initializeApp(
+    {     
+        projectId: "breadshop-e8dea"
+    }
+);    
+var db = firebase.firestore();
+function print_value(textview,Size,Price)
+{
+    Size=Number(document.getElementById(Size).value)-8;
+    var price = Price+Size*50;
+    var obj = document.getElementById(textview);
+    obj.innerText= '$'+String(price);
+}
+function storedata(Num,Name,Price)
+{ 
+    const numElement = document.getElementById(Num);
+    var ref = db.collection('fruit').doc($(Name).text());
+    var price = Number($(Price).text().slice(1));
+    console.log(price);
+    ref.get().then(function(doc) {
+        if (!doc.exists) {
+            ref.set(
+                {
+                    Total: Number(numElement.value),
+                    Price: Number(price)*Number(numElement.value),
+            })
+        }
+        else{
+            var num =doc.data().Total;
+            var Price =doc.data().Price;
+            ref.update(
+                {
+                    Total: Number(num)+Number(numElement.value),
+                    Price: (Number(price) * Number(numElement.value)) + Number(Price)
+                }
+                ).then(() => 
+                {     console.log('set data successful');});
+        }
+    });
+
+}
