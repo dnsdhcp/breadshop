0825c2b916dab7008cf2349b454d4f92ce9c1227
qseft <t107590039@ntut.org.tw>
2020-06-28 21:09:39 +0800

    no message

--- js/Animation.js
+++ js/Animation.js
@@ -13,3 +13,31 @@ $(function(){
            }
        });
    });
+$(function(){
+    // 初始化轮播
+    $(".start-slide").click(function(){
+        $("#carouselExampleIndicators").carousel('cycle');
+    });
+    // 停止轮播
+    $(".pause-slide").click(function(){
+        $("#carouselExampleIndicators").carousel('pause');
+    });
+    // 循环轮播到上一个项目
+    $(".prev-slide").click(function(){
+        $("#carouselExampleIndicators").carousel('prev');
+    });
+    // 循环轮播到下一个项目
+    $(".next-slide").click(function(){
+        $("#carouselExampleIndicators").carousel('next');
+    });
+    // 循环轮播到某个特定的帧 
+    $(".slide-one").click(function(){
+        $("#carouselExampleIndicators").carousel(0);
+    });
+    $(".slide-two").click(function(){
+        $("#carouselExampleIndicators").carousel(1);
+    });
+    $(".slide-three").click(function(){
+        $("#carouselExampleIndicators").carousel(2);
+    });
+});
